7bca171488c35b286cc35ab836cbe9c8b46c1e12
Sasha Budzko <bamaikoxygeno22@gmail.com Sasha Budzko>
2022-01-26 21:12:05 +0300

    math

--- js/math.js
+++ js/math.js
@@ -4,7 +4,34 @@
 /* 1. Примите два целых числа и отобразите их сумму
 Напишите функцию JavaScript, которая принимает два целых числа и отображает их сумму. */
 
-function getSumm(a, b) {
+/* function getSumm(a, b) {
    return document.writeln(`Summ: ${a} + ${b} = ${a + b}`);
 }
-getSumm(3, 5);
+getSumm(3, 5); */
+
+//
+/* 2. Преобразуйте двоичное число в десятичное
+Напишите функцию JavaScript для преобразования двоичного числа в десятичное.
+
+Пример ввода: document.write(bin_to_dec ('110011')); document.write(bin_to_dec ('100')); 51 4 */
+
+/* function getNumInTen(a, base) {
+   if (isNaN(parseInt(a, base))) return 0;
+   return console.log(parseInt(a, base)); //document.writeln(parseInt(a, base));
+}
+getNumInTen('110011', 2);
+getNumInTen('100', 2); */
+
+//
+/* 3. Преобразуйте десятичное число в двоичное, шестнадцатеричное и восьмеричное число
+Напишите функцию JavaScript для преобразования десятичного числа в двоичное, шестнадцатеричное или восьмеричное число.
+Пример ввода: document.write(dec_to_bho (120, 'B')); document.write(dec_to_bho (120, 'H')); document.write(document.write(120, 'O')); "1111000" "78" "170" */
+
+/* console.log(120..toString(2));
+console.log(120..toString(8));
+console.log(120..toString(16)); */
+
+
+
+
+
