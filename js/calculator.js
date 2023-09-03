c8966eb878519f143594ad5c3409fe30bec04cf6
OxygenO22 <bamaik@mail.ru>
2023-09-03 13:57:53 +0300

    Fixed done

--- js/calculator.js
+++ js/calculator.js
@@ -6,44 +6,15 @@ function calculator(string) {
   let a;
   let b;
   let symb = arr[1];
-  let arrRome = [];
 
   if (arr.length === 1) {
     throw Error('Cтрока не является математической операцией!');
   } else if (arr.length !== 3) {
     throw Error('Должно быть два операнда и один оператор!');
   };
-  
-  const defineIsRomeDigits = () => {
-    let arrIsRome = []
-    for (let key in digits) {
-      if (arr[0] === arr[2]) {
-        switch (key) {
-        case arr[0].toUpperCase():
-          arrIsRome.push(digits[key]);
-        case arr[2].toUpperCase():
-          arrIsRome.push(digits[key]);
-          break;
-        }
-      } else {
-        switch (key) {
-        case arr[0].toUpperCase():
-          arrIsRome.push(digits[key]);
-          break;
-        case arr[2].toUpperCase():
-          arrIsRome.push(digits[key]);
-          break;
-        }
-      }
-    }
-    if (arrIsRome.length === 2){
-      return true;
-    } else {
-      return false;
-    }
-  };
 
   const findRomeDigits = () => {
+    let arrRome = [];
     for (let key in digits) {
       if (arr[0] === arr[2]) {
         switch (key) {
@@ -67,13 +38,21 @@ function calculator(string) {
     return arrRome;
   };
   
+  const defineIsRomeDigits = () => {
+    if (findRomeDigits().length === 2){
+      return true;
+    } else {
+      return false;
+    }
+  };
+  
   if (defineIsRomeDigits()) {
     findRomeDigits();
-    a = arrRome[0];
-    b = arrRome[1];
+    a = findRomeDigits()[0];
+    b = findRomeDigits()[1];
   } else {
     findRomeDigits();
-    if (arrRome.length === 1) {
+    if (findRomeDigits().length === 1) {
       throw Error('Используются одновременно разные системы счисления!');
     } else {
       a = +arr[0];
@@ -137,9 +116,7 @@ function calculator(string) {
     throw Error('Допустимые значения от 1 до 10!');
   };
 
-
-
 }
 
-calculator('I - X');
-//calculator('X - I');
+//calculator('I - X');
+calculator('5 - 1');
