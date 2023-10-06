0ccacfca6a5aa3733c0d5e3e0d730e260abc2ff6
OxygenO22 <bamaik@mail.ru>
2023-10-06 19:33:08 +0300

    Aston test analysis

--- js/codewars.js
+++ js/codewars.js
@@ -56,7 +56,7 @@ console.log(dadFilter(string)) */
 
 //4. The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. Vowels are any of aeiou.
 
-const solve = (s) => {
+/* const solve = (s) => {
 	let comparisonArr = ['a', 'e', 'i', 'o', 'u', 'y'];
 	let firstArr = s.split('');
 	let count = 1; 
@@ -94,4 +94,4 @@ const solve = (s) => {
 }
 
 //console.log(solve('codewarriors'));
-console.log(solve('suoidea'));
+console.log(solve('suoidea')); */
