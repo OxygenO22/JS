64493f0ab25a8c2bb98cb92a97ae600d2db8023c
OxygenO22 <bamaik@mail.ru>
2023-11-22 17:33:22 +0300

    this 2

--- js/this.js
+++ js/this.js
@@ -1,6 +1,6 @@
 // 1
 
-const person = {
+/* const person = {
   firstName: 'John',
   lastName: 'Doe',
   age: 50,
@@ -16,4 +16,25 @@ const c = person['lastName'];
 
 for (let x in person) {
   console.log(person[x]);
-}
+} */
+
+
+// 2
+
+  /* function outer() {
+    function inner() {
+      var i = 4;
+      console.log('i + g = ', i + g);
+    }
+    var o = 3;
+    console.log('b + o = ', b + o);
+    inner();
+  }
+
+  var g = 1;
+
+  if (true) {
+    var b = 2;
+  }
+  outer(); */
+
