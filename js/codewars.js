52bee25d4fdbff1b521836ceaeeddf5e44620b4d
OxygenO22 <bamaik@mail.ru>
2024-06-17 14:37:11 +0300

    Codewars task 6 is done

--- js/codewars.js
+++ js/codewars.js
@@ -54,6 +54,7 @@ let dadFilter = (str) => {
 
 console.log(dadFilter(string)) */
 
+
 //4. The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring. Vowels are any of aeiou.
 
 /* const solve = (s) => {
@@ -97,4 +98,87 @@ console.log(dadFilter(string)) */
 console.log(solve('suoidea')); */
 
 
+//5 Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example: 
+/* 3 -->    15  (  3 * 5¹)
+ 10 -->   250  ( 10 * 5²)
+200 --> 25000  (200 * 5³)
+  0 -->     0  (  0 * 5¹)
+ -3 -->   -15  ( -3 * 5¹) */
 
+/* const multiply = (number) => {
+	const constNum = 5;
+	const numTostr = number.toString();
+	let degree;
+	if (numTostr.includes('-')) {
+		degree = numTostr.slice(1).length;
+	} else {
+		degree = numTostr.length;
+	}
+	return number * Math.pow(constNum, degree) 
+} */
+
+// or
+
+/* const multiply = (number) => number * Math.pow(5, Math.abs(number).toString().length);
+
+console.log(multiply(-2))  */
+
+
+// 6 Write a function that accepts two parameters (sum and multiply) and find two numbers [x, y], where x + y = sum and x * y = multiply.
+
+/* Example:
+
+sum = 12 and multiply = 32
+
+In this case, x equals 4 and y equals 8.
+
+x = 4
+
+y = 8
+
+Because
+
+x + y = 4 + 8 = 12 = sum
+
+x * y = 4 * 8 = 32 = multiply
+
+The result should be [4, 8].
+
+Note:
+
+0 <= x <= 1000
+
+0 <= y <= 1000
+
+If there is no solution, your function should return null (or None in python).
+
+You should return an array (list in python) containing the two values [x, y] and it should be sorted in ascending order.
+
+One last thing: x and y are integers (no decimals). */
+
+/* const sumAndMultiply = (sum, multiply) => {
+	const resArr = [];
+	const a = 1;
+	const b = sum;
+	const c = multiply;
+	
+	if ((Number.isInteger(sum) && Number.isInteger(multiply)) && (sum > 0 && multiply > 0) && (sum <= 1000 && multiply <= 1000) && (sum !== NaN && multiply !== NaN)) {
+		const x = (-(-b) + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
+		const y = (-(-b) - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
+		console.log('x: ', x);
+		console.log('y: ', y);
+		if (Number.isInteger(x) && Number.isInteger(y)) {
+			resArr.push(x, y);
+			resArr.sort((a, b) => a - b);
+			return resArr;
+		} else {
+			console.log('inner null');
+			return null;
+		}
+	} else {
+		return null;
+	}
+}
+
+console.log(sumAndMultiply(57, 80))
+console.log(sumAndMultiply(570, 839)) */
