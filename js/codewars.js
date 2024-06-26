02cfd575c325ad4d4d82b0a85b6128f901d92924
OxygenO22 <bamaik@mail.ru>
2024-06-26 16:32:10 +0300

    Codewars 6 kyu

--- js/codewars.js
+++ js/codewars.js
@@ -487,3 +487,13 @@ console.log(generateHashtag("    Hello     World   "));
 console.log(generateHashtag(""));
 console.log(generateHashtag(" ".repeat(200)));
 console.log(generateHashtag("  ")); */
+
+
+/// 14
+
+/* Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0] */
+
+/* const moveZeros = (arr) => arr.filter(i => i !== 0).concat(arr.filter(i => i === 0));
+console.log(moveZeros([1,2,0,1,0,1,0,3,0,1])); */
