05e60226fbb58f3992d80914a7e7a09e2087c304
OxygenO22 <bamaik@mail.ru>
2024-07-14 23:27:12 +0300

    CW task 23 is done

--- js/codewars.js
+++ js/codewars.js
@@ -750,3 +750,35 @@ ShortLongShort.solution("22", "1"); // returns "1221" */
 
 console.log(solution('Soon', 'Me'));
 console.log(solution('45', '1')); */
+
+
+/// 23
+
+/* Complete the function that
+
+accepts two integer arrays of equal length
+compares the value each member in one array to the corresponding member in the other
+squares the absolute value difference between those two values
+and returns the average of those squared absolute value difference between each member pair.
+Examples
+[1, 2, 3], [4, 5, 6]              -->   9   because (9 + 9 + 9) / 3
+[10, 20, 10, 2], [10, 25, 5, -2]  -->  16.5 because (0 + 25 + 25 + 16) / 4
+[-1, 0], [0, -1]                  -->   1   because (1 + 1) / 2 */
+
+/* const solution = (firstArray, secondArray) => {
+
+	const res = [];
+
+	for (let i = 0; i < firstArray.length; i++) {
+		res.push(Math.pow((firstArray[i] - secondArray[i]), 2));
+	}
+
+	return res.reduce((acc, cur) => acc + cur, 0) / firstArray.length;
+} */
+
+/// or
+
+/* const solution = (a, b) =>  a.reduce((s, n, i) => s + Math.pow(n - b[i], 2), 0) / a.length; */
+
+/* console.log(solution([1,2,3],[4,5,6]));
+console.log(solution([10,20,10,2],[10,25,5,-2])); */
